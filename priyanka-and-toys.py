8345299274e1bcd618b82e4c3416f1309dabdb60
hjevans <evanshanj@gmail.com>
2018-05-16 19:32:41 -0400

    Delete priyanka-and-toys.py

--- priyanka-and-toys.py
+++
@@ -1,8 +0,0 @@
-length = int(raw_input())
-weights = map(int, raw_input().strip().split(" "))
-cost = [0,0,0,0,0]
-for i in range(length):
-    for j in range(length):
-        if weights[i] > weights[j] or weights[i] + 4 < weights[j] or weights[i] == weights[j]:
-            cost[i] += 1
-print(min(cost))
